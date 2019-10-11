7e68c09adb0150108682a3297f9e0a5e0f4fe88c
Abel Olayinka <55124283+Inkztek@users.noreply.github.com>
2019-10-11 15:07:12 +0100

    update connection.php

--- connection.php
+++ connection.php
@@ -10,7 +10,7 @@ define('DB_NAME', 'intern_grader');
 $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
-if($connection === false){
+if($conn === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
